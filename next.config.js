d75bd431acbd6adf2faf4400aab9bf33a4f73b38
Dewa Danu Brata <dewadanubrata@gmail.com>
2024-09-22 14:27:48 +0700

    fixing build in node 14

--- next.config.js
+++ next.config.js
@@ -1,129 +1,44 @@
-const path = require('path')
-
-module.exports = {
-  staticPageGenerationTimeout: 100000,
-  trailingSlash: false,
-
-  // ignoreBuildErrors: true,
-  reactStrictMode: false,
-
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  staticPageGenerationTimeout: 1000,
+  ignoreBuildErrors: true,
   webpack5: true,
-
-  // swcMinify: true,
-  // distDir: 'build',
-
-  // output: 'export',
-  images: {
-    unoptimized: false
-
-    // unoptimized: true
+  reactStrictMode: false,
+  // transpilePackages: ['react-hook-mousetrap'],
+  eslint: {
+    ignoreDuringBuilds: true
   },
-  compiler: {
-    // removeConsole: true,
-    // styledComponents: true,
-    // removeConsole: {
-    //   exclude: ['error']
-    // }
-  },
-
-  // transpilePackages: ['mui-one-time-password-input'],
-
-  // transpilePackages: ['@mui/x-charts', 'react-hook-mousetrap'],
-  // transpilePackages: ['@mui/x-charts'],
-  // eslint: {
-  //   ignoreDuringBuilds: true
-  // },
   // experimental: {
-  //   transpilePackages: ['@mui/x-charts']
-  // },
-  // modularizeImports: {
-  //   '@mui/material': {
-  //     transform: '@mui/material/{{member}}'
-  //   },
-  //   '@mui/x-charts': {
-  //     transform: '@mui/x-charts/{{member}}'
-  //   }
-  // },
-
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }) => {
-    if (cfg.cache && !dev) {
-      cfg.cache = Object.freeze({
-        type: 'memory'
-      })
-      cfg.cache.maxMemoryGenerations = 0
-    }
-
-    // Important: return the modified config
-    return config
-  },
-
+  // cpus: 1,
+  // workerThreads: false
+  // }
   experimental: {
-    webpackBuildWorker: true
-
-    //   esmExternals: false,
-    //   jsconfigPaths: false, // enables it for both jsconfig.json and tsconfig.json
-    //   optimizePackageImports: [
-    //     'lodash',
-    //     '@mui/material',
-    //     '@mui/icons-material',
-    //     'store',
-    //     'yup',
-    //     'next/router',
-    //     'crypto-js',
-    //     'primereact/datatable',
-    //     'primereact/column',
-    //     'store',
-    //     'react-number-format',
-    //     'moment',
-    //     'libphonenumber-js',
-    //     'react-countdown'
-    //   ]
-  },
-  webpack: config => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      apexcharts: path.resolve(__dirname, './node_modules/apexcharts-clevision')
-    }
-
-    return config
+    optimizePackageImports: [
+      'lodash',
+      '@mui/material',
+      '@mui/icons-material',
+      'store',
+      'yup',
+      'next/router',
+      'crypto-js',
+      '@sentry/nextjs',
+      'mdi-material-ui',
+      'mui-daterange-picker',
+      'primereact/datatable',
+      'primereact/column',
+      'store',
+      'react-number-format',
+      'moment',
+      'libphonenumber-js',
+      'mui-otp-input-field',
+      'next',
+      'react',
+      'react-dom',
+      'react-countdown',
+      'react-apexcharts',
+      'apexcharts'
+    ]
   }
 }
 
-// Injected content via Sentry wizard below
-
-const { withSentryConfig } = require('@sentry/nextjs')
-
-module.exports = withSentryConfig(module.exports, {
-  // For all available options, see:
-  // https://github.com/getsentry/sentry-webpack-plugin#options
-
-  org: 'digipayid',
-  project: 'digipayid-fe',
-
-  // Only print logs for uploading source maps in CI
-  silent: !process.env.CI,
-
-  // For all available options, see:
-  // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-
-  // Upload a larger set of source maps for prettier stack traces (increases build time)
-  widenClientFileUpload: true,
-
-  // Uncomment to route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-  // This can increase your server load as well as your hosting bill.
-  // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
-  // side errors will fail.
-  // tunnelRoute: "/monitoring",
-
-  // Hides source maps from generated client bundles
-  hideSourceMaps: true,
-
-  // Automatically tree-shake Sentry logger statements to reduce bundle size
-  disableLogger: true,
-
-  // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
-  // See the following for more information:
-  // https://docs.sentry.io/product/crons/
-  // https://vercel.com/docs/cron-jobs
-  automaticVercelMonitors: true
-})
+module.exports = nextConfig
